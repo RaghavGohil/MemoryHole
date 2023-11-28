6a4d6b6b761e2653c1691268fe3e7f86bc19a0d8
Raghav <raghavgohil2004@gmail.com>
2023-11-28 17:44:14 +0530

    added some sprites and started working on collision!

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import math
 from my_math import *
 from blocks import * 
 import pygame
@@ -10,15 +11,15 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (0,0)
         self.move_speed= 5 
-        self.move_time = 3
-        self.lerp_time = 0
+        self.collision_move_amt = 5
     
     def set_player(self,pos:tuple)->None:
         self.rect.topleft = pos 
     
     def update(self,framed_delta:float,change_level):
-        self.__move_player(framed_delta)
         self.__hole_collision_level_change(change_level)
+        self.__wall_collision()
+        self.__move_player(framed_delta)
 
     def draw(self,win:pygame.surface.Surface)->None:
         win.blit(self.image, self.rect)
@@ -26,18 +27,18 @@ class Player(pygame.sprite.Sprite):
     def __move_player(self,framed_delta:float)->None: #framed_delta = framed_delta * framerate
         keys = pygame.key.get_pressed()
         direction = [0,0]
-        if keys[pygame.K_UP] or keys[pygame.K_w]: 
-            direction[1] -= 1
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            direction[1] += 1
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]: 
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]): 
             direction[0] -= 1
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]: 
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]): 
             direction[0] += 1
+        if (keys[pygame.K_UP] or keys[pygame.K_w]): 
+            direction[1] -= 1
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]):
+            direction[1] += 1
         
         if not vec_is_zero(direction):
-            normalized_pos = vec_mul(vec_normalize(direction),self.move_speed*framed_delta)
-            self.rect.topleft = tuple(vec_add(list(self.rect.topleft),normalized_pos))
+            direction_speed_vec = vec_mul(vec_normalize(direction),self.move_speed*framed_delta)
+            self.rect.topleft = tuple(vec_add(list(self.rect.topleft),direction_speed_vec))
         
     def __hole_collision_level_change(self, change_level)->None: # change level callback actually calls the generate level in game
         collide = False
@@ -52,8 +53,17 @@ class Player(pygame.sprite.Sprite):
             change_level() # changes the level itself so there is no need to put checks for collision
 
     def __wall_collision(self)->None:
-        pass
-
+        for wall in WallBlock.container:
+            print(f"{wall.rect.right - self.rect.left >= 0} {wall.rect.left - self.rect.right <= 0} {wall.rect.bottom - self.rect.top >= 0} {wall.rect.top - self.rect.bottom <= 0}")
+            if self.rect.colliderect(wall.rect):
+                if wall.rect.right - self.rect.left >= 0: # left 
+                    self.rect.left += self.collision_move_amt
+                if wall.rect.left - self.rect.right <= 0: # right
+                    self.rect.left -= self.collision_move_amt
+                if wall.rect.bottom - self.rect.top >= 0: # top 
+                    pass
+                if wall.rect.top - self.rect.bottom <= 0: # bottom 
+                    pass
 
     def draw_and_update_sprite(self,win:pygame.surface.Surface,framed_delta:float,change_level)->None: #for learning about the arguments visit functions above
         self.update(framed_delta,change_level)
